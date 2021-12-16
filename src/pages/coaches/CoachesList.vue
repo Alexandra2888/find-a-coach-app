b78b1606903dbc81dbe2ccb04ad0c193a74aace5
Alexandra2888 <moldovan.alexandra28@gmail.com>
2021-12-16 17:03:38 +0200

    updating coachesList

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -3,9 +3,32 @@
     FILTER
   </section>
   <section>
-    LIST OF COACHES
+    <div class="controls">
+      <buttton>Refresh</buttton>
+      <router-link to="/register">Register as Coach</router-link>
+    </div>
+    <ul v-if="hasCoaches">
+      <li v-for="coach in filteredCoaches" :key="coach.id">
+        {{ coach.firstName }}
+      </li>
+      </ul>
+      <h3 v-else>No coaches found</h3>
   </section>
 </template>
+
+<script>
+
+export default ({
+  computed: {
+    filteredCoaches() {
+      return this.$store.getters['coaches/coaches'];
+    },
+    hasCoaches() {
+      return this.$store.getters['coaches/hasCoaches'];
+    },
+  },
+});
+</script>
 
 <style>
 </style>
