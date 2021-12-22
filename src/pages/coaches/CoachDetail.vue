a4de5b539463d2a62e514e8e49fc67fe524a9c6a
Alexandra2888 <moldovan.alexandra28@gmail.com>
2021-12-22 17:28:00 +0200

    adding coachDetail

--- src/pages/coaches/CoachDetail.vue
+++ src/pages/coaches/CoachDetail.vue
@@ -1,4 +1,49 @@
+/* eslint-disable comma-spacing */
 <template>
-  DETAILS FOR COACH
-  <router-link to="/coaches/c1/contact">Contaxt</router-link>
+ <section>
+   <base-card>
+   <h2>{{ fullName }}</h2>
+   <h3>{{ rate }}/hour</h3>
+     </base-card>
+ </section>
+ <section>
+   <base-card>
+   <header>
+     <h2>Interested? Reach out now!</h2>
+   <base-button link :to="contactLink">Contact
+   </base-button>
+   </header>
+   <router-view></router-view>
+   </base-card>
+ </section>
+ <section>
+   <base-card>
+   <base-badge v-for="area in areas" :key="area" :bind="type" :title="area"></base-badge>
+   <p>{{ description }}</p>
+   </base-card>
+ </section>
 </template>
+
+<script>
+export default {
+  props: ['id'],
+  data() {
+    return {
+      selectedCoach: null,
+    };
+  },
+  computed: {
+    fullName() {
+      // eslint-disable-next-line prefer-template
+      return this.selectedCoach.firstName + '' + this.selectedCoach.lastName;
+    },
+    areas() {
+      return this.selectedCoach.areas;
+    },
+    contactLink() {
+      // eslint-disable-next-line prefer-template
+      return this.$router.path + '/' + this.id + '/contact';
+    },
+  },
+};
+</script>
