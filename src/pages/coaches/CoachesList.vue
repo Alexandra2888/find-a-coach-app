b6ce963a0b8e240817454af92493d331157f2467
Alexandra2888 <moldovan.alexandra28@gmail.com>
2021-12-22 07:36:52 +0200

    adding base-card

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -1,24 +1,33 @@
 <template>
-  <section>
-    FILTER
-  </section>
+  <section>FILTER</section>
   <section>
     <div class="controls">
       <buttton>Refresh</buttton>
       <router-link to="/register">Register as Coach</router-link>
     </div>
     <ul v-if="hasCoaches">
-      <li v-for="coach in filteredCoaches" :key="coach.id">
-        {{ coach.firstName }}
-      </li>
-      </ul>
-      <h3 v-else>No coaches found</h3>
+      <coach-item
+        v-for="coach in filteredCoaches"
+        :key="coach.id"
+        :id="coach.id"
+        :first-name="coach.firstName"
+        :lastName="coach.lastName"
+        :rate="coach.hourlyRate"
+        :areas="coach.areas"
+      >
+      </coach-item>
+    </ul>
+    <h3 v-else>No coaches found</h3>
   </section>
 </template>
 
 <script>
+import CoachItem from '../../components/coaches/CoachItem.vue';
 
-export default ({
+export default {
+  components: {
+    CoachItem,
+  },
   computed: {
     filteredCoaches() {
       return this.$store.getters['coaches/coaches'];
@@ -27,8 +36,18 @@ export default ({
       return this.$store.getters['coaches/hasCoaches'];
     },
   },
-});
+};
 </script>
 
-<style>
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.controls {
+  display: flex;
+  justify-content: space-between;
+}
 </style>
